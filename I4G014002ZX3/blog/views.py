2a74dbd181492eca03a6145b310cd9a21caaa7d5
calebameh <105277579+calebameh@users.noreply.github.com>
2022-06-26 07:55:34 +0100

    Initial commit

--- I4G014002ZX3/blog/views.py
+++ I4G014002ZX3/blog/views.py
@@ -10,25 +10,33 @@ from django.views import generic
 
 class PostListView (generic.ListView):
     model = Post
-    # template_name = 'base.html'
+    template_name = 'blog/post_list.html'
 
 
 class PostCreateView (generic.CreateView):
     model = Post
     fields = "__all__"
     success_url = reverse_lazy("blog:all")
+    template_name = 'blog/post_form.html'
+
 
 class PostDetailView (generic.DetailView):
     model = Post
+    template_name = 'blog/post_detail.html'
+
 
 class PostUpdateView (generic.UpdateView):
     model = Post
     fields = "__all__"
     success_url = reverse_lazy("blog:all")
+    template_name = 'blog/post_form.html'
 
-class PostDeleteView (generic.UpdateView):
+
+class PostDeleteView (generic.DeleteView):
     model = Post
     fields = "__all__"
     success_url = reverse_lazy("blog:all")
+    template_name = 'blog/post_confirm_delete.html'
+
 
 
